af2f33c658306a57eab1610355b7f66a6f971af9
anthonyprudhomme <anthony.prudhomme@epfedu.fr>
2018-06-14 17:19:25 +0200

    Added company service functions

--- src/app/company/company.service.ts
+++ src/app/company/company.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Company } from './company.model';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CompanyService {
 
-  constructor() { }
+  private _baseUrl = 'http://10.0.1.55:8080/api/v1/recipes';
+  id: number;
+  constructor(private http: HttpClient) { }
+
+  getCompanies(): Observable<Company[]> {
+    return this.http.get<Company[]>(this._baseUrl);
+  }
+
+  getCompany(id: string): Observable<Company> {
+    return this.http.get<Company>(`${ this._baseUrl }/${ id }`);
+  }
+
+  postCompany(recipe: Company): Observable<Company> {
+    return this.http.post<Company>(this._baseUrl, recipe);
+  }
+
+  updateCompany(company: Company): Observable<Company> {
+    return this.http.post<Company>(this._baseUrl, company);
+  }
+
+  deleteCompany(recipe: Company): Observable<Company> {
+    return this.http.delete<Company>(`${ this._baseUrl }/${ recipe.id }`);
+  }
 }
---
+++ src/app/computer/computer.model.ts
@@ -0,0 +1,8 @@
+export class Computer {
+  id: number;
+  name: string;
+  introduced: Date;
+  discontinued: Date;
+  manufacturer: string;
+  manufacturerId: number;
+}
